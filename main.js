e24ab1decac3d8818756a126da49303354c64e83
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-04-30 11:25:11 -0400

    added comments

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Display the numbers from 1-20.
+//01. Display the numbers from 1-20.
 function one() {
     let answer = "";
     for (let i = 1; i <= 20; i++) {
@@ -10,7 +10,7 @@ function one() {
     element.textContent = answer;
     document.body.appendChild(element);
 }
-//Display the even numbers up to 20.
+//02. Display the even numbers up to 20.
 function two() {
     let answer = "";
     for (let i = 1; i <= 20; i++) {
@@ -22,7 +22,7 @@ function two() {
     element.textContent = answer;
     document.body.appendChild(element);
 }
-//Display the odd numbers up to 20.
+//03. Display the odd numbers up to 20.
 function three() {
     let answer = "";
     for (let i = 1; i <= 20; i++) {
@@ -34,7 +34,7 @@ function three() {
     element.textContent = answer;
     document.body.appendChild(element);
 }
-//Display the multiples of 5 up to 100.
+//04. Display the multiples of 5 up to 100.
 function four() {
     let answer = "";
     for (let i = 1; i <= 100; i++) {
@@ -46,7 +46,7 @@ function four() {
     element.textContent = answer;
     document.body.appendChild(element);
 }
-//Display the square numbers up to 100.
+//05. Display the square numbers up to 100.
 function five() {
     let answer = "";
     for (let i = 1; i <= 100; i++) {
@@ -59,7 +59,7 @@ function five() {
     element.textContent = answer;
     document.body.appendChild(element);
 }
-//Display the numbers counting back from 20.
+//06. Display the numbers counting back from 20.
 function six() {
     let answer = "";
     for (let i = 20; i > 0; i--) {
@@ -69,7 +69,7 @@ function six() {
     element.textContent = answer;
     document.body.appendChild(element);
 }
-//Display the even numbers decending from 20.
+//07. Display the even numbers decending from 20.
 function seven() {
     let answer = "";
     for (let i = 20; i > 0; i--) {
@@ -82,7 +82,7 @@ function seven() {
     document.body.appendChild(element);
 
 }
-//Display the odd numbers counting down from 20.
+//08. Display the odd numbers counting down from 20.
 function eight() {
     let answer = "";
     for (let i = 20; i > 0; i--) {
@@ -94,7 +94,7 @@ function eight() {
     element.textContent = answer;
     document.body.appendChild(element);
 }
-//display the multiples of five decending from 100.
+//09. display the multiples of five decending from 100.
 function nine() {
     let answer = "";
     for (let i = 100; i > 0; i--) {
@@ -107,7 +107,7 @@ function nine() {
     document.body.appendChild(element);
 
 }
-//display the square numbers counting down to from 100.
+//10. display the square numbers counting down to from 100.
 function ten() {
     let answer = "";
     for (let i = 10; i > 0; i--) {
@@ -124,28 +124,28 @@ function ten() {
 
 //setting the sample array as a const.
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
-// display the array to page.
+//11. display the array to page.
 function eleven() {
     let s = sampleArray;
     var element = document.createElement("div");
     element.textContent = s;
     document.body.appendChild(element);
 }
-// display the even numbers of the array.
+//12. display the even numbers of the array.
 function evens() {
     let evens = sampleArray.filter(i => i % 2 === 0);
     var element = document.createElement("div");
     element.textContent = evens;
     document.body.appendChild(element);
 }
-// dispaly the odd numbers of the array
+//13. dispaly the odd numbers of the array
 function odds() {
     let odds = sampleArray.filter(i => i & 2 != 0);
     var element = document.createElement("div");
     element.textContent = odds;
     document.body.appendChild(element);
 }
-// display the numbers from the array squared.
+//14. display the numbers from the array squared.
 function square() {
     let square = sampleArray.map(function (a) {
         return a * a;
@@ -155,7 +155,7 @@ function square() {
     document.body.appendChild(element);
 }
 
-// display the sum of the numbers 1-20
+//15. display the sum of the numbers 1-20
 function sum(){
     let numbers =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
     let sum = numbers.reduce(function(a, b) {
@@ -166,7 +166,7 @@ function sum(){
     document.body.appendChild(element);
 }
 
-// display the sum of all the numbers in the given array
+//16. display the sum of all the numbers in the given array
 function sum2() {
     let sum2 = sampleArray.reduce(function(a, b) {
         return a + b;})
@@ -175,7 +175,7 @@ function sum2() {
     document.body.appendChild(element);
 }
 
-//find smallest number in sample array
+//17. find smallest number in sample array
 function small() {
     let small = Math.min.apply(Math, sampleArray);
     var element = document.createElement("div");
@@ -183,13 +183,15 @@ function small() {
     document.body.appendChild(element);
 }
 
-//find the largest number in the sample array
+//18. find the largest number in the sample array
 function largest() {
     let large = Math.max.apply(Math, sampleArray);
     var element = document.createElement("div");
     element.textContent = large;
     document.body.appendChild(element);
 }
+
+//19. creating the rectangles
 one()
 two()
 three()
